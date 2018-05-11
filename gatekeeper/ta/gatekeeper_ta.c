42eb12ca15e52d83a5c6f2e447e324b153d0eba3
Joakim Bech <joakim.bech@linaro.org>
2018-05-11 13:18:48 +0200

    Add all functions as stubs from aosp gatekeeper.h
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- gatekeeper/ta/gatekeeper_ta.c
+++ gatekeeper/ta/gatekeeper_ta.c
@@ -9,6 +9,9 @@
 #include <tee_internal_api_extensions.h>
 #include <tee_internal_api.h>
 
+/*******************************************************************************
+ * Public functions in the gatekeeper.h in AOSP
+ ******************************************************************************/
 static TEE_Result enroll(uint32_t __unused param_types, TEE_Param __unused params[4])
 {
 	TEE_Result res = TEE_SUCCESS;
@@ -25,6 +28,200 @@ static TEE_Result enroll(uint32_t __unused param_types, TEE_Param __unused param
 
 	return res;
 }
+
+static TEE_Result verify(uint32_t __unused param_types, TEE_Param __unused params[4])
+{
+	TEE_Result res = TEE_SUCCESS;
+
+	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE,
+						   TEE_PARAM_TYPE_NONE,
+						   TEE_PARAM_TYPE_NONE,
+						   TEE_PARAM_TYPE_NONE);
+
+	if (param_types != exp_param_types) {
+		EMSG("Expected: 0x%x, got: 0x%x", exp_param_types, param_types);
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	return res;
+}
+
+/*******************************************************************************
+ * Protected functions in the gatekeeper.h in AOSP
+ ******************************************************************************/
+static bool GetAuthTokenKey(const uint8_t **auth_token_key, uint32_t *length)
+{
+	// FIXME: Implementation
+	(void)auth_token_key;
+	(void)length;
+	return false;
+}
+
+static void GetPasswordKey(const uint8_t **password_key, uint32_t *length)
+{
+	// FIXME: Implementation
+	(void)password_key;
+	(void)length;
+}
+
+// FIXME: Implement salt_t
+typedef int salt_t;
+
+static void ComputePasswordSignature(
+	uint8_t *signature, uint32_t signature_length,
+	const uint8_t *key, uint32_t key_length,
+	const uint8_t *password, uint32_t password_length,
+	salt_t salt)
+{
+	// FIXME: Implementation
+	(void)signature;
+	(void)signature_length;
+	(void)key;
+	(void)key_length;
+	(void)password;
+	(void)password_length;
+	(void)salt;
+}
+
+static void GetRandom(void *random, uint32_t requested_size)
+{
+	// FIXME: Implementation
+	(void)random;
+	(void)requested_size;
+}
+
+static void ComputeSignature(uint8_t *signature, uint32_t signature_length,
+			     const uint8_t *key, uint32_t key_length,
+			     const uint8_t *message, const uint32_t length)
+{
+	// FIXME: Implementation
+	(void)signature;
+	(void)signature_length;
+	(void)key;
+	(void)key_length;
+	(void)message;
+	(void)length;
+}
+
+static uint64_t GetMillisecondsSinceBoot(void)
+{
+	// FIXME: Implementation
+	return 0;
+}
+
+// FIXME: Implement secure_id_t
+typedef int secure_id_t;
+
+// FIXME: Implement failure_record_t
+typedef int failure_record_t;
+
+static bool GetFailureRecord(uint32_t uid, secure_id_t user_id,
+			     failure_record_t *record, bool secure)
+{
+	// FIXME: Implementation
+	(void)uid;
+	(void)user_id;
+	(void)record;
+	(void)secure;
+	return false;
+}
+
+static bool ClearFailureRecord(uint32_t uid, secure_id_t user_id, bool secure)
+{
+	// FIXME: Implementation
+	(void)uid;
+	(void)user_id;
+	(void)secure;
+	return false;
+}
+
+static uint32_t ComputeRetryTimeout(const failure_record_t *record)
+{
+	// FIXME: Implementation
+	(void)record;
+	return 0;
+}
+
+static bool IsHardwareBacked(void)
+{
+	return true;
+}
+
+// FIXME: Implement password_handle_t
+typedef uint32_t password_handle_t;
+
+// FIXME: Implement SizedBuffer
+typedef uint8_t SizedBuffer;
+
+static bool DoVerify(const password_handle_t *expected_handle,
+		     const SizedBuffer *password)
+{
+	// FIXME: Implementation
+	(void)expected_handle;
+	(void)password;
+	return false;
+}
+
+/*******************************************************************************
+ * Private functions in the gatekeeper.h in AOSP
+ ******************************************************************************/
+static void MintAuthToken(uint8_t *auth_token, uint32_t *length,
+			  uint64_t timestamp, secure_id_t user_id,
+			  secure_id_t authenticator_id, uint64_t challenge)
+{
+	// FIXME: Implementation
+	(void)auth_token;
+	(void)length;
+	(void)timestamp;
+	(void)user_id;
+	(void)authenticator_id;
+	(void)challenge;
+}
+
+static bool CreatePasswordHandle(
+	SizedBuffer *password_handle, salt_t salt, secure_id_t secure_id,
+	secure_id_t authenticator_id, uint8_t handle_version,
+	const uint8_t *password, uint32_t password_length)
+{
+	// FIXME: Implementation
+	(void)password_handle;
+	(void)salt;
+	(void)secure_id;
+	(void)authenticator_id;
+	(void)handle_version;
+	(void)password;
+	(void)password_length;
+	return false;
+}
+
+static bool IncrementFailureRecord(uint32_t uid, secure_id_t user_id, uint64_t timestamp,
+				   failure_record_t *record, bool secure)
+{
+	// FIXME: Implementation
+	(void)uid;
+	(void)user_id;
+	(void)timestamp;
+	(void)record;
+	(void)secure;
+	return false;
+}
+
+// FIXME: Implement GateKeeperMessage
+typedef uint8_t GateKeeperMessage;
+
+static bool ThrottleRequest(uint32_t uid, uint64_t timestamp,
+			    failure_record_t *record,
+			    bool secure, GateKeeperMessage *response)
+{
+	// FIXME: Implementation
+	(void)uid;
+	(void)timestamp;
+	(void)record;
+	(void)secure;
+	(void)response;
+	return false;
+}
+
 
 /*******************************************************************************
  * Mandatory TA functions.
@@ -63,6 +260,9 @@ TEE_Result TA_InvokeCommandEntryPoint(void __unused *sess_ctx,
 	switch (cmd_id) {
 	case TA_GATEKEEPER_ENROLL:
 		return enroll(param_types, params);
+
+	case TA_GATEKEEPER_VERIFY:
+		return verify(param_types, params);
 
 	default:
 		return TEE_ERROR_BAD_PARAMETERS;
--- gatekeeper/ta/sub.mk
+++ gatekeeper/ta/sub.mk
@@ -1,2 +1,4 @@
 global-incdirs-y += include
 srcs-y += gatekeeper_ta.c
+# FIXME: Remove the -Wno-unused-function when all functions have been implemented
+cflags-y += -Wno-unused-function
