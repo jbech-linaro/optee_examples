21ff3df8bafe0c1916f6af7b2985b59494a512b9
Joakim Bech <joakim.bech@linaro.org>
2018-05-25 12:40:07 +0200

    ComputeRetryTimeout implemented
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- gatekeeper/ta/gatekeeper_ta.c
+++ gatekeeper/ta/gatekeeper_ta.c
@@ -164,11 +164,38 @@ static bool ClearFailureRecord(uint32_t uid, secure_id_t user_id, bool secure)
 	return false;
 }
 
+/*
+ * Calculates the timeout in milliseconds as a function of the failure
+ * counter 'x' as follows:
+ *
+ * [0. 5) -> 0
+ * 5 -> 30
+ * [6, 10) -> 0
+ * [11, 30) -> 30
+ * [30, 140) -> 30 * (2^((x - 30)/10))
+ * [140, inf) -> 1 day
+ *
+ * TODO: Function more or less a copy/paste from AOSP, license!
+ */
 static uint32_t ComputeRetryTimeout(const struct failure_record_t *record)
 {
-	// FIXME: Implementation
-	(void)record;
-	return 0;
+	static const int failure_timeout_ms = 30000;
+	if (record->failure_counter == 0)
+		return 0;
+
+	if (record->failure_counter > 0 && record->failure_counter <= 10) {
+		if (record->failure_counter % 5 == 0) {
+			return failure_timeout_ms;
+		}  else {
+			return 0;
+		}
+	} else if (record->failure_counter < 30) {
+		return failure_timeout_ms;
+	} else if (record->failure_counter < 140) {
+		return failure_timeout_ms << ((record->failure_counter - 30) / 10);
+	}
+
+	return DAY_IN_MS;
 }
 
 static bool IsHardwareBacked(void)
