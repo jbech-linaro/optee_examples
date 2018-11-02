97bcf99e804dc698067e60ebf497fbe89c66d298
Joakim Bech <joakim.bech@linaro.org>
2018-11-02 15:19:22 +0100

    template: Add template TA
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

---
+++ scripts/template/host/main.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) <<<YEAR>>>, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+
+/* OP-TEE TEE client API (built by optee_client) */
+#include <tee_client_api.h>
+
+/* To the the UUID (found the the TA's h-file(s)) */
+#include <<<<TA_NAME_LOWER_CASE>>>_ta.h>
+
+int main(void)
+{
+	TEEC_Context ctx = { 0 };
+	TEEC_Operation op = { 0 };
+	TEEC_Result res = TEE_ERROR_BAD_PARAMETERS;
+	TEEC_Session sess = { 0 };
+	TEEC_UUID uuid = TA_<<<TA_NAME_UPPER_CASE>>>_UUID;
+	uint32_t err_origin = 0;
+
+	/* Initialize a context connecting us to the TEE */
+	res = TEEC_InitializeContext(NULL, &ctx);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+
+	/* Open a session to the <<<TA_NAME_LOWER_CASE>>> TA. */
+	res = TEEC_OpenSession(&ctx, &sess, &uuid, TEEC_LOGIN_PUBLIC, NULL,
+			       NULL, &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
+		     res, err_origin);
+
+	/*
+	 * Prepare the argument. Pass a value in the first parameter, the
+	 * remaining three parameters are unused.
+	 */
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+	op.params[0].value.a = 1336;
+
+	/*
+	 * TA_<<<TA_NAME_UPPER_CASE>>>_CMD_INC_VALUE is the actual function in
+	 * the TA to be called.
+	 */
+	printf("Invoking TA to increment %d\n", op.params[0].value.a);
+	res = TEEC_InvokeCommand(&sess,
+				 TA_<<<TA_NAME_UPPER_CASE>>>_CMD_INC_VALUE,
+				 &op, &err_origin);
+
+	if (res != TEEC_SUCCESS) errx(1, "TEEC_InvokeCommand failed with code
+				      0x%x origin 0x%x", res, err_origin);
+
+	printf("TA incremented value to %d\n", op.params[0].value.a);
+
+	/*
+	 * We're done with the TA, close the session and destroy the context.
+	 */
+
+	TEEC_CloseSession(&sess);
+
+	TEEC_FinalizeContext(&ctx);
+
+	return 0;
+}
---
+++ scripts/template/ta/include/template_ta.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) <<<YEAR>>>, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TA_<<<TA_NAME_UPPER_CASE>>>_H
+#define TA_<<<TA_NAME_UPPER_CASE>>>_H
+
+#define TA_<<<TA_NAME_UPPER_CASE>>>_UUID \
+	<<<UUID_HEADERFILE>>>
+#if 0
+	{ 0xffffffff, 0xffff, 0xffff, { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff} }
+#endif
+
+/* The function IDs implemented in this Trusted Application */
+#define TA_<<<TA_NAME_UPPER_CASE>>>_CMD_INC_VALUE	0
+#define TA_<<<TA_NAME_UPPER_CASE>>>_CMD_DEC_VALUE	1
+
+#endif
