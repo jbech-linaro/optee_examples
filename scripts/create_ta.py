2fe2f34145658af5773d8d8e0263f0289d210ced
Joakim Bech <joakim.bech@linaro.org>
2018-11-02 20:15:46 +0100

    Dry-run and replace works
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- scripts/create_ta.py
+++ scripts/create_ta.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 
+import datetime
 import os
 import re
 import subprocess
 import sys
-import tempfile
 import uuid
 
 verbose = False
+dry_run = False
 
 ###############################################################################
 # Argument parser
@@ -28,6 +29,10 @@ def get_parser():
                         default=None,
                         help='Specific UUID to use')
 
+    parser.add_argument('-d', '--dry-run', required=False, action="store_true",
+                        default=False,
+                        help='Just output to stdout, do any sed operations')
+
     parser.add_argument('-v', required=False, action="store_true",
                         default=False,
                         help='Output some verbose debugging info')
@@ -41,6 +46,7 @@ def get_parser():
 
 def convert_to_header_format(my_uuid):
     global verbose
+
     if verbose:
         print("time_low: {:08x}".format(my_uuid.time_low))
         print("time_mid: {:04x}".format(my_uuid.time_mid))
@@ -60,7 +66,7 @@ def convert_to_header_format(my_uuid):
     # that.
     # { 0xffffffff, 0xffff, 0xffff,
     #    { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff} }
-    return "{{ 0x{:08x}, 0x{:04x}, 0x{:04x}," \
+    return "{{ 0x{:08x}, 0x{:04x}, 0x{:04x}, " \
            "{{ 0x{:02x}, 0x{:02x}, {} }} }}".format(
             my_uuid.time_low,
             my_uuid.time_mid,
@@ -81,45 +87,12 @@ def create_uuid(my_uuid):
 
     return uuid_makefile, uuid_headerfile
 
-###############################################################################
-# Main function
-###############################################################################
 
-
-def main(argv):
-    parser = get_parser()
-
-    args = parser.parse_args()
-
+def sedify(word_to_replace, ta_name, destination):
+    global dry_run
     global verbose
-    if args.v:
-        print("Enable verbose")
-        verbose = True
 
-    if not args.name:
-        print("No Trusted Application name provided\n")
-        parser.print_help()
-        sys.exit(os.EX_USAGE)
-
-    ta_name_upper_case = args.name.upper()
-    ta_name_lower_case = args.name.lower()
-    print("TA_NAME_UPPER_CASE: {}".format(ta_name_upper_case))
-    print("TA_NAME_LOWER_CASE: {}".format(ta_name_lower_case))
-
-    uuid_makefile, uuid_headerfile = create_uuid(args.uuid)
-    print("UUID_MAKEFILE: {}".format(uuid_makefile))
-    print("UUID_HEADERFILE: {}".format(uuid_headerfile))
-
-    root = os.path.split(os.getcwd())[0]
-    dest = "{}/{}".format(root, ta_name_lower_case)
-    source = "{}/template".format(os.getcwd())
-
-    if verbose:
-        print("src: {}".format(source))
-        print("dst: {}".format(dest))
-
-    # subprocess.call(["cp", "-r", source, dest])
-    p1 = subprocess.Popen(["rgrep", "-l", "TA_NAME_UPPER_CASE", dest],
+    p1 = subprocess.Popen(["rgrep", "-l", word_to_replace, destination],
             stdout=subprocess.PIPE)
 
     output = p1.communicate()[0].decode("utf-8")
@@ -128,11 +101,82 @@ def main(argv):
     output.remove('')
 
     for f in output:
-        keyword_from = "<<<TA_NAME_UPPER_CASE>>>"
-        keyword_to = ta_name_upper_case
-        sed_args = "-i s'/{}/{}/'".format(keyword_from, keyword_to)
-        print("sed {} {}".format(sed_args, f))
-        #subprocess.call(["sed", "-i s'/<<<TA#_NAME_UPPER_CASE>>>/", ta_name_upper_case, "/",
+        sed_args = "s/<<<{}>>>/{}/".format(word_to_replace, ta_name)
+
+        if dry_run:
+            p2 = subprocess.Popen(["sed", sed_args, f], stdout=subprocess.PIPE)
+            if verbose:
+                print("\nsed {} {}".format(sed_args, f))
+        else:
+            p2 = subprocess.Popen(["sed", "-i", sed_args, f], stdout=subprocess.PIPE)
+
+        output = p2.communicate()[0].decode("utf-8")
+        # if verbose:
+        #    print(output)
+
+
+###############################################################################
+# Main function
+###############################################################################
+
+
+def main(argv):
+    global verbose
+    global dry_run
+
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.v:
+        print("Enable verbose")
+        verbose = True
+
+    if args.dry_run:
+        print("Running dry-run (no sed changes)")
+        dry_run = True
+
+    if not args.name:
+        print("No Trusted Application name provided\n")
+        parser.print_help()
+        sys.exit(os.EX_USAGE)
+
+    # Prepare TA name
+    ta_name_upper_case = args.name.upper()
+    ta_name_lower_case = args.name.lower()
+    if verbose:
+        print("TA_NAME_UPPER_CASE: {}".format(ta_name_upper_case))
+        print("TA_NAME_LOWER_CASE: {}".format(ta_name_lower_case))
+
+    # Prepare UUIDs
+    uuid_makefile, uuid_headerfile = create_uuid(args.uuid)
+    if verbose:
+        print("UUID_MAKEFILE: {}".format(uuid_makefile))
+        print("UUID_HEADERFILE: {}".format(uuid_headerfile))
+
+    # Prepare year for copyright
+    now = datetime.datetime.now()
+
+    # Calculate source and destination path
+    root = os.path.split(os.getcwd())[0]
+    dest = "{}/{}".format(root, ta_name_lower_case)
+    source = "{}/template".format(os.getcwd())
+
+    if verbose:
+        print("src: {}".format(source))
+        print("dst: {}".format(dest))
+
+    # Copy the template
+    # subprocess.call(["cp", "-r", source, dest])
+
+    conv_array = {
+            ("TA_NAME_UPPER_CASE", ta_name_upper_case),
+            ("TA_NAME_LOWER_CASE", ta_name_lower_case),
+            ("UUID_MAKEFILE", uuid_makefile),
+            ("UUID_HEADERFILE", uuid_headerfile),
+            ("YEAR", now.year) }
+
+    for i in conv_array:
+        sedify(i[0], i[1], dest)
 
 
 if __name__ == "__main__":
