6cec1ff9e35513733488acc311395335c1db915f
Joakim Bech <joakim.bech@linaro.org>
2019-06-12 15:40:44 +0200

    aes-gcm: Initial commit
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

---
+++ aes_gcm/ta/include/aes-gcm_ta.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TA_AES_GCM_H
+#define TA_AES_GCM_H
+
+#define AES_GCM_TA_UUID \
+	{ 0x9a8bc80b, 0xa3ff, 0x42db, { 0xb2, 0xe0, 0x80, 0xa9, 0x31, 0xc9, 0x7c, 0xe4 } }
+
+/* The function IDs implemented in this Trusted Application */
+#define TA_AES_GCM_CMD_INC_VALUE	0
+#define TA_AES_GCM_CMD_DEC_VALUE	1
+
+#endif
